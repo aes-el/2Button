51fa517b5f2f314642499616c0b216e2b84b1279
LoneTony <anthonyleone@theleones.net>
2026-02-19 14:03:06 -0500

    Put Things Together In SampleScene
    Put the assets we made together into the Sample Scene for a build.

--- Assets/Scripts/BouncyBall.cs
+++ Assets/Scripts/BouncyBall.cs
@@ -4,6 +4,7 @@ public class BouncyBall : MonoBehaviour
 {
     public float minY = -5.5f;
     private Rigidbody2D rb;
+    public float maxVelocity = 15f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,5 +19,18 @@ public class BouncyBall : MonoBehaviour
             transform.position = Vector3.zero;
             rb.linearVelocity = Vector2.zero;
         }
+
+        if (rb.linearVelocity.magnitude > maxVelocity)
+        {
+            rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
+        }
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+ {
+     if (collision.gameObject.CompareTag("Brick"))
+     {
+         Destroy(collision.gameObject);
+     }
+ }
 }
