f69f4213e00029df0696ab8bf6e9892bc7afb156
aes-el <aes.elmiller@gmail.com>
2026-02-19 13:47:52 -0500

    Brick + Generator
    Script for brick and level generator semi-complete. Both should work, but the offset for the generator doesn't want to cooperate.   - Aesop

---
+++ Assets/AesopContent/BrickAesop.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BrickAesop : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+// Insert below to ball script
+
+// private void OnCollisionEnter2D(Collision2D collision)
+// {
+//     if (collision.gameObject.CompareTag("Brick"))
+//     {
+//         Destroy(collision.gameObject);
+//     }
+// }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/AesopContent/LevelGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Vector2Int size;
+    public Vector2Int offset;
+    public GameObject brickPrefab;
+    public Gradient gradient;
+
+    private void Awake()
+    {
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                // automate brick placement
+                GameObject newBrick = Instantiate(brickPrefab, transform);
+                newBrick.transform.position = transform.position + new Vector3((float)((size.x-1)*.5f-i) * offset.x, j * offset.y, 0);
+                //brick color change
+                newBrick.GetComponent<SpriteRenderer>().color = gradient.Evaluate((float)j/(size.y - 1));
+            }
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
