610399e42948d10baa307d0e7320fe04aebe0ca7
LoneTony <anthonyleone@theleones.net>
2026-02-19 15:11:11 -0500

    Put My Stuff in a Folder and Added Paddle Speed
    Put my prefabs and other stuff into a folder and edited the paddle script to temporarily speed up with a spacebar click.

--- Assets/AnthonyContent/Scripts/PlayerMovement.cs
+++ Assets/AnthonyContent/Scripts/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private float movementHorizontal;
     public float maximumX = 7.5f;
+    public InputActionReference spaceBar;
+    private int timer;
     void Start()
     {
         
@@ -18,6 +21,22 @@ public class PlayerMovement : MonoBehaviour
             (movementHorizontal < 0 && transform.position.x > -maximumX))
         {
             transform.position += Vector3.right * speed * movementHorizontal * Time.deltaTime;
+        }
+
+        if (spaceBar.action.WasPressedThisFrame())
+        {
+            speed = speed * 1.5f;
+        }
+
+        timer += 1;
+        
+        if (speed > 15 && timer % 10 == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                speed = speed - 0.05f;
+            }
+            
         }
     }
 }
